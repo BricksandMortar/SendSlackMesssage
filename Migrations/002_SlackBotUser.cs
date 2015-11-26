9035acf6d853e498baca8e55bc4dc94dbd8cee3b
Arran France <arranf@users.noreply.github.com>
2015-11-26 16:50:15 +0000

    Add full access token to defined type migration and GUID to Guid class

--- Migrations/002_SlackBotUser.cs
+++ Migrations/002_SlackBotUser.cs
@@ -15,9 +15,10 @@ namespace com.bricksandmortarstudio.Slack.Migrations
         {
             RockMigrationHelper.DeleteAttribute( "BF026984-C330-4EBB-879C-2C2AC671FA6D" ); // Webhook
             RockMigrationHelper.DeleteDefinedType( "495981C4-BD0E-4C9A-9FA1-50E07DB2DAB8" ); // Slack
-            RockMigrationHelper.AddDefinedType( "Communication", "Slack", "Bot Users for use in Slack", "495981C4-BD0E-4C9A-9FA1-50E07DB2DAB8", @"Set up Bot Users by visiting by visiting yourdomain.slack.com > Configure Integrations > Bot Users" );
+            RockMigrationHelper.AddDefinedType( "Communication", "Slack Users", "Bots and fully authenticated users and tokens for use in Slack", "495981C4-BD0E-4C9A-9FA1-50E07DB2DAB8", @"Set up Bot Users by visiting by visiting yourdomain.slack.com > Configure Integrations > Bot Users" );
             RockMigrationHelper.AddDefinedTypeAttribute( "495981C4-BD0E-4C9A-9FA1-50E07DB2DAB8", "9C204CD0-1233-41C5-818A-C5DA439445AA", "Token", "Token", "The API token provided by Slack. This can be found under Slack > Integrations > Bot Users > Integration Settings > API Token", 43, "", "84B96E9D-C012-4D49-A586-7CFD4C28EEA7" );
             RockMigrationHelper.AddAttributeQualifier( "84B96E9D-C012-4D49-A586-7CFD4C28EEA7", "ispassword", "False", "2BFA30A0-51F9-4CC1-BBE2-2B85279DB7D8" );
+            RockMigrationHelper.AddDefinedValue( "495981C4-BD0E-4C9A-9FA1-50E07DB2DAB8", "Fully Authenticated User", "The token for this fully authenticated user can be obtained from api.slack.com/web. WARNING: Using this full access token will associate all actions with the team admin.", "BEFBCC3B-88E0-4E28-BD1E-D2E32D23C9D0", true );
         }
 
         public override void Down()
--- SystemGuid/SlackGuid.cs
+++ SystemGuid/SlackGuid.cs
@@ -6,6 +6,8 @@
         /// Gets the Slack Defined Type guid
         /// </summary>
         public const string SLACK_BOTUSERS_DEFINEDTYPE = "495981C4-BD0E-4C9A-9FA1-50E07DB2DAB8";
+        public const string SLACK_FULL_ACCESS_TOKEN = "BEFBCC3B-88E0-4E28-BD1E-D2E32D23C9D0";
     }
 }
+
 
